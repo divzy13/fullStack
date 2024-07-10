b50c012d1597937e1f40e2c18ad36cfbbbfc321b
divrav1 <divya.raveendran1@inter.ikea.com>
2024-07-10 12:46:41 +0200

    updated function name

--- azure/src/main/java/com/users/Function.java
+++ azure/src/main/java/com/users/Function.java
@@ -20,7 +20,7 @@ public class Function {
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("get-all-users")
+    @FunctionName("get-all-users-test")
     public HttpResponseMessage run(
             @HttpTrigger(
                 name = "req",
